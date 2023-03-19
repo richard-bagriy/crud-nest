53625c29dd0a12f290858761814ba5d88e059b5e
Richard Bagriy <richard.bagriy@gmail.com>
2023-03-20 00:27:14 +0200

    added role by default for all users

--- backend/src/users/interfaces/user-details.interface.ts
+++ backend/src/users/interfaces/user-details.interface.ts
@@ -1,7 +1,10 @@
+import { Roles } from "../enum/roles.enum";
+
 export interface UserDetails {
     id: string;
     email: string;
     username: string;
+    role: Roles;
     createdAt?: Date;
     updatedAt?: Date;
     refreshToken?: string;
