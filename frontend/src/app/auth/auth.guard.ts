b96635291b4f120b6ff8fd3520408638edfe6550
Richard Bagriy <richard.bagriy@gmail.com>
2023-03-21 20:51:54 +0200

    fixed redirects

--- frontend/src/app/auth/auth.guard.ts
+++ frontend/src/app/auth/auth.guard.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private readonly authService: AuthService, private readonly router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      const user = this.authService.currentUserValue;
+
+      if (user) {
+        return true;
+      }
+
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }})
+    return false;
   }
   
 }
